41bc4f1b6c8390d7bc28c52c3bac91608c20a263
Dragos Rogojan <dragos.rogojan@gmail.com>
2023-06-30 14:38:17 +0300

    Refactor - extracted the instance of the StringCalculator

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,9 +1,10 @@
 import {StringCalculator} from "./stringCalculator.js";
 
 describe('StringCalculator', () => {
+    const stringCalculator = new StringCalculator();
+
     it('should return 0 when give an empty string', () => {
         // Arrange
-        const stringCalculator = new StringCalculator();
         // Act
         const sum = stringCalculator.add("");
         // Assert
@@ -11,7 +12,6 @@ describe('StringCalculator', () => {
     })
 
     it('should return the number when given a single number', function () {
-        const stringCalculator = new StringCalculator();
         expect(stringCalculator.add("4")).toEqual(4);
         expect(stringCalculator.add("10")).toEqual(10);
     });
