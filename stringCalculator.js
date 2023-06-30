0195981cc4cc5c3c2078507a7dd58d3271ed4141
Dragos Rogojan <dragos.rogojan@gmail.com>
2023-06-30 14:37:11 +0300

    Adding a single number returns the number

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,8 @@
 export class StringCalculator {
     add(numbers) {
-        return 0;
+        if(numbers === "") {
+            return 0;
+        }
+        return parseInt(numbers);
     }
 }
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -9,4 +9,10 @@ describe('StringCalculator', () => {
         // Assert
         expect(sum).toEqual(0);
     })
+
+    it('should return the number when given a single number', function () {
+        const stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("4")).toEqual(4);
+        expect(stringCalculator.add("10")).toEqual(10);
+    });
 })
